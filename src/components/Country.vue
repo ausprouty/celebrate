ce8ef7c32460484c208f056bb588ba8c33b67c82
Bob Prouty <bob.prouty@powertochange.org.au>
2019-01-25 12:19:10 +1100

    before adjusting bookmark to start of new page.

--- src/components/Country.vue
+++ src/components/Country.vue
@@ -1,75 +1,73 @@
 <template>
-<div class="app-link" v-on:click="newBookmark(country)">
-<div class="country-card -shadow">
-<img v-bind:src="appDir.country+ country.image" class="flag">
-<div class="country-names">
-<span class="country-name">{{country.name}}</span>
-<br/>
- <span class="country-name-english">{{country.english}}</span>
-</div>
-</div>
-</div>
+  <div class="app-link" v-on:click="newBookmark(country)">
+    <div class="country-card -shadow">
+      <img v-bind:src="appDir.country+ country.image" class="flag">
+      <div class="country-names">
+        <span class="country-name">{{country.name}}</span>
+        <br>
+        <span class="country-name-english">{{country.english}}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 export default {
-props: {
-country: Object
-},
-computed: mapState(['bookmark', 'appDir']),
-methods: {
-newBookmark: function(country) {
-console.log('New Bookmark for ')
-console.log(country)
-this.$store
-.dispatch('newBookmark', country)
-.then(() => {
-this.$router.push({
-name: 'languages',
-params: { countryCODE: country.code }
-})
-})
-.catch(() => {
-console.log('There was a problem storing country')
-})
-}
-}
+  props: {
+    country: Object
+  },
+  computed: mapState(['bookmark', 'appDir']),
+  methods: {
+    newBookmark: function(country) {
+      console.log('New Bookmark for ')
+      console.log(country)
+      this.$store
+        .dispatch('newBookmark', country)
+        .then(() => {
+          this.$router.push({
+            name: 'languages',
+            params: { countryCODE: country.code }
+          })
+        })
+        .catch(() => {
+          console.log('There was a problem storing country')
+        })
+    }
+  }
 }
 </script>
 
 <style scoped>
-
 div.break {
-display: inline;
+  display: inline;
 }
 
 .country-card {
-cursor: pointer;
-margin-bottom: 12px;
-padding: 10px;
-text-align: left;
-transition: all 0.2s linear;
-width: 95%;
+  background-color:#efefef;
+  cursor: pointer;
+  margin-bottom: 12px;
+  padding: 10px;
+  text-align: left;
+  transition: all 0.2s linear;
+  width: 95%;
 }
 div.country-names {
-float: right;
-font-size: 18px;
-vertical-align: top;
-width: 70%;
+  float: right;
+  font-size: 18px;
+  vertical-align: top;
+  width: 70%;
 }
-.country-name{
-font-weight:bold;
-line-height: 20px;
+.country-name {
+  font-weight: bold;
+  line-height: 20px;
 }
-.country-name-english{
-font-weight:normal;
-line-height: 20px;
+.country-name-english {
+  font-weight: normal;
+  line-height: 20px;
 }
 
 img.flag {
-width: 25%;
+  width: 25%;
 }
-
-
 </style>
