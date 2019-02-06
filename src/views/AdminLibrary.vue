28e96129115f9d93068b8ace94bb995948ce7604
Bob Prouty <bob.prouty@powertochange.org.au>
2019-02-07 10:19:35 +1100

    Series works

--- src/views/AdminLibrary.vue
+++ src/views/AdminLibrary.vue
@@ -6,47 +6,63 @@
     <div class="content" v-if="loaded">
       <h1>Library</h1>
       <div>
-
-        <div v-for="(country, index) in countries" :key="country.code" :country="country">
+        <button class="button" @click="addNewBookForm">New Book</button>
+        <div v-for="(book, index) in library" :key="book.code" :book="book">
           <div class="app-card -shadow">
-            <div class="float-right" style="cursor:pointer" @click="deleteCountryForm(index)">X</div>
-            <h4 class="card-title">Country #{{index}}</h4>
+            <div class="float-right" style="cursor:pointer" @click="deleteBookForm(index)">X</div>
+            <h4 class="card-title">Book #{{index}}</h4>
             <div class="form">
-              <span>Country Name :</span>
+              <span>Title:</span>
+              <input type="text" class="form-control mb-2" placeholder="Title" v-model="book.title">
+              <br>
+              <br>
+              <img v-bind:src="appDir.library  + image_dir  + '/' + book.image" class="book">
+              <br>
+              <span>Image:</span>
               <input
                 type="text"
                 class="form-control mb-2"
-                placeholder="Country Name"
-                v-model="country.name"
+                placeholder="myfriends"
+                v-model="book.image"
               >
-              <span>English Name:</span>
+              
+              <span>Book:</span>
+              <select v-model="book.book">
+                <option value="issues">Issues</option>
+                <option value="basics">Basics</option>
+                <option value="community">Community</option>
+                <option value="firststeps">First Steps</option>
+                <option value="compass">Compass</option>
+                <option value="about">About</option>
+              </select>
+              
+              <span>Folder:</span>
               <input
                 type="text"
                 class="form-control mb-2"
-                placeholder="English Name"
-                v-model="country.english"
+                placeholder="myfriends"
+                v-model="book.folder"
               >
-              <span>country 2 letter ISO:</span>
+              <span>Format:</span>
+              <select v-model="book.format">
+                <option value="series">series</option>
+                <option value="page">page</option>
+              </select>
+              <div v-if="book.format == 'series'">
+                <span>Index</span>
+                <input
+                  type="text"
+                  class="form-control mb-2"
+                  placeholder="basics-chapters.json"
+                  v-model="book.index"
+                >
+              </div>
+              <span>Style Sheet</span>
               <input
                 type="text"
-                maxlength="2"
                 class="form-control mb-2"
-                placeholder="2 letter ISO code"
-                v-model="country.code"
-              >
-              <span>Index</span>
-              <input
-                type="text"
-                class="form-control mb-2"
-                placeholder="index-ISO"
-                v-model="country.english"
-              >
-              <span>Image</span>
-              <input
-                type="text"
-                class="form-control mb-2"
-                placeholder="ISO.png"
-                v-model="country.image"
+                placeholder="AU-myfriends.css"
+                v-model="book.style"
               >
             </div>
           </div>
@@ -65,7 +81,8 @@ export default {
   components: {
     NavBar
   },
-  computed: mapState(['bookmark', 'appDir']),
+  props: ['countryCODE', 'languageISO'],
+  computed: mapState(['bookmark', 'appDir', 'cssURL', 'standard']),
   data() {
     return {
       library: [
@@ -77,36 +94,123 @@ export default {
           index: '',
           style: 'AU-myfriends.css',
           image: 'issues.jpg',
-          format: 'series',
-          instructions: ''
+          format: 'series'
         }
       ],
+      image_dir: 'menu-europe',
       loading: false,
       loaded: null,
       error: null
     }
   },
- 
+  methods: {
+    addNewBookForm() {
+      this.library.push({
+        id: '',
+        book: '',
+        title: '',
+        folder: '',
+        index: '',
+        style: 'AU-myfriends.css',
+        image: 'issues.jpg',
+        format: 'series'
+      })
+    },
+    deleteBookForm(index) {
+      this.library.splice(index, 1)
+    }
+  },
   created() {
-    /* Update bookmark based on this route (for people to select URL frcountriesom another source)
-       Bookmark stores current Country and all specialized info for that country
-      If there is only one country for this country we then go back to country
-    */
     this.error = this.loaded = null
     this.loading = true
-    this.countries = []
-    console.log('about to get countries ')
-    DataService.getLibrary()
-      .then(response => {
-        console.log('response data')
-        console.log(response.data)
-        this.countries = response.data
-        this.loaded = true
-        this.loading = false
-      })
-      .catch(() => {
-        console.log('There was a problem finding countries')
-      })
+    var route = {}
+    route.country = this.$route.params.countryCODE
+    route.language = this.$route.params.languageISO
+    this.$store.dispatch('checkBookmark', route).then(response => {
+      // it is safer to get data each time tha rely on bookmark
+      DataService.getLibrary(route.country, route.language)
+        .then(response => {
+          this.library = response.data
+          if (typeof this.bookmark.language != 'undefined') {
+            console.log('USING BOOKMARK')
+            console.log(this.bookmark.language)
+            this.image_dir = this.bookmark.language.image_dir
+            console.log(this.image_dir)
+          } else {
+            console.log('USING STANDARD')
+            this.image_dir = this.standard.image_dir
+          }
+          this.loading = false
+          this.loaded = true
+        })
+        .catch(error => {
+          this.library = [
+            {
+              id: 1,
+              book: 'issues',
+              title: 'Life Issues',
+              folder: 'myfriends',
+              index: 'principle-chapters.json',
+              style: 'AU-myfriends.css',
+              image: 'issues.jpg',
+              format: 'series'
+            },
+            {
+              id: 2,
+              book: 'basics',
+              title: 'Basic Conversations',
+              folder: 'myfriends',
+              index: 'basics-chapters.json',
+              style: 'AU-myfriends.css',
+              image: 'basics.jpg',
+              format: 'series'
+            },
+            {
+              id: 3,
+              book: 'community',
+              title: 'Live Community',
+              folder: 'myfriends',
+              page: 'community',
+              style: 'AU-myfriends.css',
+              image: 'community.jpg',
+              format: 'page'
+            },
+            {
+              id: 4,
+              book: 'firststeps',
+              title: 'First Steps',
+              folder: 'first_steps',
+              index: 'first_steps-chapters.json',
+              style: 'AU-fsteps.css',
+              image: 'firststeps.jpg',
+              format: 'series'
+            },
+            {
+              id: 5,
+              book: 'compass',
+              title: 'Compass',
+              folder: 'compass',
+              index: 'compass-chapters.json',
+              style: 'AU-compass.css',
+              image: 'compass.jpg',
+              format: 'series'
+            },
+            {
+              id: 6,
+              book: 'about',
+              title: 'About MyFriends',
+              folder: 'myfriends',
+              page: 'community',
+              style: 'AU-myfriends.css',
+              image: 'about.jpg',
+              format: 'page'
+            }
+          ]
+          this.image_dir = this.standard.image_dir
+          this.loading = false
+          this.loaded = true
+        })
+    })
   }
 }
 </script>
--- src/views/AdminSeries.vue
+++ src/views/AdminSeries.vue
@@ -1,15 +1,148 @@
 <template>
-    <div>
-
+  <div>
+    <NavBar/>
+    <div class="loading" v-if="loading">Loading...</div>
+    <div class="error" v-if="error">There was an error...</div>
+    <div class="content" v-if="loaded">
+      <h1>Series for {{this.$route.params.countryCODE}}</h1>
+       <div class="form">
+            <span>Description:</span>
+            <br>
+            <textarea v-model="seriesDetails.description" placeholder="add multiple lines"></textarea>            
+        </div>
+      <div>
+        <button class="button" @click="addNewChapterForm">New Chapter</button>
+        <div v-for="(chapter, index) in chapters" :key="chapter.id" :chapter="chapter">
+          <div class="app-card -shadow">
+            <div class="float-right" style="cursor:pointer" @click="deleteChapterForm(index)">X</div>
+            <h4 class="card-title">Chapter #{{chapter.id}}</h4>
+            <div class="form">
+              <span>Title:</span>
+              <input
+                type="text"
+                class="form-control mb-2"
+                placeholder="Title"
+                v-model="chapter.title"
+              >
+              <span>Description:</span>
+              <input
+                type="text"
+                class="form-control mb-2"
+                placeholder="Description"
+                v-model="chapter.description"
+              >
+               <span>Chapter Number:</span>
+              <input
+                type="text"
+                class="form-control mb-2"
+                placeholder="leave blank for un-numbered items"
+                v-model="chapter.count"
+              >
+              <span>Filename:</span>
+              <input
+                type="text"
+                class="form-control mb-2"
+                placeholder=""
+                v-model="chapter.filename"
+              >
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+import { mapState } from 'vuex'
+import Chapter from '@/components/Chapter.vue'
+import DataService from '@/services/DataService.js'
+import NavBar from '@/components/NavBarAdmin.vue'
+export default {
+  props: ['countryCODE', 'languageISO', 'bookNAME'],
+  computed: mapState(['bookmark', 'appDir']),
+  components: {
+    Chapter,
+    NavBar
+  },
+  data() {
+    return {
+      seriesDetails: {
+        series: '',
+        language: '',
+        description: ''
+      },
+      chapter: {},
+      chapters: [
+        {
+          id: '',
+          title: '',
+          desciption: '',
+          count: '',
+          filename: ''
+        }
+      ],
+      dir: 'ltr',
+      loadinG: false,
+      loading: false,
+      loaded: null,
+      error: null
     }
+  },
+  methods: {
+    addNewChapterForm() {
+      this.chapters.push({
+        id: '',
+        title: '',
+        description: '',
+        count: '',
+        filename: ''
+      })
+    },
+    deleteChapterForm(id) {
+      this.chapters.splice(id, 1)
+    }
+  },
+  created() {
+    this.error = this.loaded = null
+    this.loading = true
+    var route = {}
+    route.country = this.$route.params.countryCODE
+    route.language = this.$route.params.languageISO
+    route.book = this.$route.params.bookNAME // we need book to get style sheet
+    route.series = this.$route.params.bookNAME
+    this.$store.dispatch('checkBookmark', route).then(response => {
+      DataService.getSeries(
+        route.country,
+        route.language,
+        this.bookmark.book.folder,
+        this.bookmark.book.index
+      )
+        .then(response => {
+          console.log(response.data) // For nseriesDetailsow, logs out the response
+          this.seriesDetails = response.data
+          console.log('this.seriesDetails')
+          console.log(this.seriesDetails)
+
+          this.chapters = this.seriesDetails.chapters
+          console.log('chapters in Series.Vue')
+          console.log(this.chapters)
+          this.loading = false
+          this.loaded = true
+        })
+        .catch(error => {
+          this.loading = false
+          console.log('There was an error:', error.response) // Logs out the error
+          this.error = error.toString()
+        })
+    })
+  }
+}
 </script>
 
-<style scoped>
 
+<style scoped>
+.float-right {
+  text-align: right;
+}
 </style>
