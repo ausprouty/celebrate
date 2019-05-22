7256d8967010e585d486b2f68c0f4324af871ba5
ausprouty <bob.prouty@powertochange.org.au>
2019-05-22 13:16:56 +1000

    vue js restored

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
+// see http://vuejs-templates.github.io/webpack for documentation.
+
 module.exports = {
   // crossorigin:
   //   '<script src="http://192.168.56.1:8080" crossorigin="anonymous"></script>',
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  outputDir: process.env.NODE_ENV === 'production' ? '/prototype/' : '/'
 }
