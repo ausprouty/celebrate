bfb9807e92d91015bc1445b30dfd4cc68c2bf10f
ausprouty <bob.prouty@powertochange.org.au>
2019-05-22 13:18:19 +1000

    vue js fixed

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,5 @@
 module.exports = {
   // crossorigin:
   //   '<script src="http://192.168.56.1:8080" crossorigin="anonymous"></script>',
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-  outputDir: process.env.NODE_ENV === 'production' ? '/prototype/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 }
