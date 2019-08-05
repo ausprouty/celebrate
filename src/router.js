a0bdea5801e86366c553b1284a0ab544e2d952ba
ausprouty <bob.prouty@powertochange.org.au>
2019-08-06 09:03:41 +1000

    attempted controlling size

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Content from './views/Content.vue'
+
 import CountriesEdit from './views/CountriesEdit.vue'
 import CountriesPreview from './views/CountriesPreview.vue'
 import CountriesSort from './views/CountriesSort.vue'
@@ -53,6 +55,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: CountriesPreview
+    },
+    {
+      path: '/content/:country?/:language?/:folder?/:filename?',
+      name: 'content',
+      component: Content
     },
 
     {
---
+++ src/views/Content.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>{{ this.page }}</div>
+</template>
+<script>
+export default {
+  props: ['country_code', 'language_iso', 'folder_name', 'filename'],
+  data() {
+    return {
+      page: ''
+    }
+  },
+
+  async created() {
+    try {
+      await this.CheckBookmarks(this.$route.params)
+      var response = await ContentService.getContent(this.$route.params)
+      this.countries = response.data.content.text
+    } catch (error) {
+      console.log('There was an error in Countries.vue:', error) // Logs out the error
+    }
+  }
+}
+</script>
+
+<style></style>
